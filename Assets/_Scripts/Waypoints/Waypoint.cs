1da2e91764b8a592ce6a9bb4f641387bac79b142
GonDie2709 <goncalves.die@gmail.com>
2020-10-30 23:57:53 -0300

    - Addin waypoints

---
+++ Assets/_Scripts/Waypoints/Waypoint.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    public int id;
+    public Waypoint[] Neighbors;
+}
---
+++ Assets/_Scripts/Waypoints/WaypointManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointFinder : Singleton<MonoBehaviour>
+{
+    [SerializeField] Transform _waypointContainer;
+    Transform[] _waypoints;
+
+
+    private void Start()
+    {
+        _waypoints = new Transform[_waypointContainer.childCount];
+
+        for (int i = 0; i < _waypointContainer.childCount; i++)
+        {
+            _waypoints[i].name = i.ToString();
+            _waypoints[i] = _waypointContainer.GetChild(i);
+        }
+    }
+
+    public List<Waypoint> FindPath(Waypoint fromWaypoint, Waypoint toWaypoint)
+    {
+        List<Waypoint> path = new List<Waypoint>();
+
+        for(int i = 0; i < fromWaypoint.Neighbors.Length; i++)
+        {
+            if(fromWaypoint.Neighbors[i] == toWaypoint)
+            { }
+        }
+
+
+
+        return path;
+    }
+}
